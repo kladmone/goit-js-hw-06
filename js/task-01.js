a55141347c68ea737176c22e2e926dedd0e35b56
klodmone <kladmone54@gmail.com>
2023-10-03 19:45:19 +0200

    firts task

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const categoriesList = document.querySelectorAll("#categories .item");
+console.log(`Number of categories: ${categoriesList.length}`);
+categoriesList.forEach((category) => {
+  const categoryName = category.querySelector("h2").textContent;
+  const categoryElement = category.querySelectorAll("ul li").length;
+  console.log(`Category: ${categoryName}`);
+  console.log(`Elements: ${categoryElement}`);
+});
